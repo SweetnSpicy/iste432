eac68a3dc498a3a19068f2e52ef91cd30cda82b6
Regina Locicero <rtl3971@rit.edu>
2018-11-06 10:41:37 -0500

    Added comments to API Grabber class

--- classes/api_grabber.php
+++ classes/api_grabber.php
@@ -6,17 +6,35 @@ require 'game.php';
 
 use GuzzleHttp\Client;
 
+/**
+ * Class ApiGrabber
+ * Is the class that opens a connection to the API, grabs data, and loads it into model classes
+ * API docs: https://boardgamegeek.com/wiki/page/BGG_XML_API2
+ * It uses Guzzle to connect to the API and make calls: http://docs.guzzlephp.org/en/stable/
+ */
 class ApiGrabber {
 
     public $client;
 
+    /**
+     * ApiGrabber constructor.
+     * Opens a connection to the API
+     */
     public function __construct() {
         $this->client = new Client([
             // Base URI is used with relative requests
             'base_uri' => 'https://www.boardgamegeek.com/xmlapi2/',
         ]);
     }
-    
+
+    /**
+     * Searches games using the inputted string
+     * Hits the API using the input then parses the returned XML into GameAbbrv objects
+     *
+     * @param $searchinput String to use in search
+     * @return array Made of GameAbbrv objects
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function searchBoardGames($searchinput) {
         $response = $this->client->request('GET', 'search?query='.$searchinput);
         //echo $response->getStatusCode();
@@ -35,6 +53,14 @@ class ApiGrabber {
         return $games;
     }
 
+    /**
+     * Uses the given game ID to find more details about the game
+     * Hits the API using the id then parses the returned XML into Game objects
+     *
+     * @param $id String of the number id of the game
+     * @return Game|null Game object containing details about the game
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function getGameByID($id) {
         $response = $this->client->request('GET', 'thing?id='.$id);
         //echo $response->getStatusCode();

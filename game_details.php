49ee6c5f064858697b08fe3c4fa2b55cc965fdc5
Regina Locicero <rtl3971@rit.edu>
2018-12-09 14:41:33 -0500

    Fixed game details layout

--- game_details.php
+++ game_details.php
@@ -2,27 +2,37 @@
 // Displays the details for a single game
 include "assets/inc/main_header.php";
 require_once __DIR__.'/classes/api_grabber.php';
+$api = new ApiGrabber();
+// TODO: grab id from a post dynamically
+$game = $api->getGameByID("131357");
+
 ?>
 
 <div class="container" style="padding-top: 85px;">
-    <div class="row">
+    <div class="row mb-4">
         <div class="col">
             <?php
-                $api = new ApiGrabber();
-                // TODO: grab id from a post dynamically
-                $game = $api->getGameByID("131357");
-                $gameOut = "
+                echo "<img src='{$game->image}' alt='{$game->name}' class='img-responsive mx-auto d-block'>";
+            ?>
+        </div>
+        <div class="col">
+            <?php
+                echo "
                     <h1>{$game->name}</h1>
-                    <p>{$game->description}</p>
-                    <p>Year Published: {$game->yearPublished}</p>
-                    <p>Minimum Players: {$game->minPlayers}</p>
-                    <p>Maximum Players: {$game->maxPlayers}</p>
-                    <p>Average Playtime: {$game->avgPlaytime}</p>
-                    <p>Minimum Playtime: {$game->minPlaytime}</p>
-                    <p>Maximum Playtime: {$game->maxPlaytime}</p>
-                    <p>Min Age: {$game->minAge}</p>
+                    <h3>Players: {$game->minPlayers} - {$game->maxPlayers}</h3>
+                    <h3>Ages {$game->minAge}+</h3>
+                    <h3>Playtime: {$game->minPlaytime} - {$game->maxPlaytime}</h3>
+                    <h3>Average Playtime: {$game->avgPlaytime}</h3>
+                    <h3>Year Published: {$game->yearPublished}</h3>
                 ";
-                echo $gameOut;
+            ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <h2>Description: </h2>
+            <?php
+                echo "<p>{$game->description}</p>";
             ?>
         </div>
     </div>

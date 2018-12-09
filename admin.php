acd4ed85e829d84e0ed06302224330876a7fecac
sricci20 <ricci.steven@ymail.com>
2018-12-09 14:08:38 -0500

    admin page

--- admin.php
+++ admin.php
@@ -6,19 +6,26 @@ include "assets/inc/nav.php";
 include "assets/inc/container.php";
 
 if(!isset($_SESSION['role'])){
-    header("Location: login.php");
+		header("Location: login.php");
+}
+elseif(isset($_SESSION['role'])){
+	if($_SESSION['role'] != 'Admin'){
+		header("Location: login.php");
+	}
 }
 
 ?>
+
 <div class="row justify-content-center align-items-center">
   <div class="col-lg-4">
-    <h1 style="text-align:center;margin-top:10px;">Boardgames</h1>
+    <h1 style="text-align:center;margin-top:10px;">Admin Home</h1>
   </div>
 </div>
 <div class='row justify-content-center align-items-center'>
   <div class='col-lg-12'>
   </div>
 </div>
+
 
 <?php
  include "assets/inc/end-container.php";
